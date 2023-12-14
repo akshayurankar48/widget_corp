20540b93a29f424b4a9e9576fa970c930ea1c181
evilieswithin <evilieswithin>
2023-12-14 09:56:36 +0530

    add files staff.php and public.css

--- staff.php
+++ staff.php
@@ -5,10 +5,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Widget Corp</title>
+    <link href="stylesheets/public.css" rel="stylesheet" href="" media="all" type="text/css" />
 </head>
 
 <body>
-
+    <div id="header">
+        <h1>Widget Corp</h1>
+    </div>
+    <div id="main">
+        <table id="structure">
+            <tr>
+                <td id="navigation">
+                    &nbsp;
+                </td>
+                <td id="page">
+                    <h2>Staff Menu</h2>
+                    <p>Welcome to the staff area.</p>
+                    <ul>
+                        <li><a href="content.php">Manage Website Content</a></li>
+                        <li><a href="new_user.php">Add Staff User</a></li>
+                        <li><a href="logout.php">Logout</a></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div id="footer">Copyright 2007, Widget Corp</div>
 </body>
 
 </html>
